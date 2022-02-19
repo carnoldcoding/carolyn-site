67516fe999c6245bf3ada4481a2e14da1d85358c
carnoldcoding <carnold1@ithaca.edu>
2022-02-19 18:43:25 -0500

    Put about section information back

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -9,8 +9,8 @@ const LandingPage = () => {
   return (
       <>
         <HeroSection/>
-        <AboutSection/>
-        <CardSection {...information}/>
+        <AboutSection {...information}/>
+        <CardSection />
         <ServiceSection {...services}/>
       </>
   )

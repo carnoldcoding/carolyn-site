4ef1bc7deb2ff2b989eb44232dbbea293c0a1455
carnoldcoding <carnoldcoding@gmail.com>
2023-04-21 21:15:13 -0400

    project-form

--- src/components/ProjectDetailForm/ProjectDetailStyles.js
+++ src/components/ProjectDetailForm/ProjectDetailStyles.js
@@ -1,20 +1,29 @@
 import styled from 'styled-components'; 
 
 export const FormContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+    min-width: 300px;
+    width: 800px;
+    margin: auto;
+    header{
+        display: flex;
+        gap:  2rem;
 
-export const FormHeader = styled.div`
-    display: flex;
-    justify-content: center;
-`
+        .form-image{
+            background-color: lightpink;
+            flex: 50%;
+        }
+        .form-details-wrapper{
+            flex: 50%;
+            h1{
+                color: #CF980A;
+                text-decoration: underline;
+                margin-bottom: 10px;
+            }
+            p{
+                color: #206D91;
+                font-size: 1.2rem;
+            }
+        }
+    }
 
-export const FormIcon = styled.div`
-    height: 200px;
-    width: 200px;
-    background: lightpink;
-`
-
-export const FormInfo = styled.div`
 `
--- src/components/ProjectDetailForm/index.js
+++ src/components/ProjectDetailForm/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { FormContainer, FormHeader, FormIcon, FormInfo } from "./ProjectDetailStyles"
+import { FormContainer } from './ProjectDetailStyles'
 
 const ProjectDetailForm = () => {
   return (
       <>
-        <FormContainer>
-        <FormHeader>
-        <FormIcon/>
-        <FormInfo>
-      <h1>Project Details Form</h1>
-      <p>Please fill out the information form below and hit "submit" to send these details to Carolyn personally.</p>
-      </FormInfo>
-      </FormHeader>
-      </FormContainer>
+          <FormContainer>
+            <header>
+               <div class="form-image">SSDC</div> 
+                <div class="form-details-wrapper">
+                    <h1>Project Details Form</h1>
+                    <p>Please fill out the information form below and hit "submit" to send these details to Carolyn personally</p>
+                </div>
+            </header>
+          </FormContainer>
       </>
   )
 }

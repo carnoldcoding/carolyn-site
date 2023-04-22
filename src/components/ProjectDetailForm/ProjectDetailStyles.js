8637400e110cd56364f6b57fa7e38ff6144b8f37
carnoldcoding <carnoldcoding@gmail.com>
2023-04-21 22:05:16 -0400

    style updates

--- src/components/ProjectDetailForm/ProjectDetailStyles.js
+++ src/components/ProjectDetailForm/ProjectDetailStyles.js
@@ -2,15 +2,23 @@ import styled from 'styled-components';
 
 export const FormContainer = styled.div`
     min-width: 300px;
-    width: 800px;
+    max-width: 800px;
     margin: auto;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    padding: 0 1rem;
+
     header{
         display: flex;
         gap:  2rem;
-
         .form-image{
-            background-color: lightpink;
             flex: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            img{
+                height: 120px;
+            }
         }
         .form-details-wrapper{
             flex: 50%;
@@ -23,6 +31,22 @@ export const FormContainer = styled.div`
                 color: #206D91;
                 font-size: 1.2rem;
             }
+        }
+    }
+    form{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 2rem;
+        margin-top: 30px;
+        input{
+            padding: 0 1rem;
+            height: 40px;
+            border-radius: 10px;
+            flex-basis: 43%;
+            outline: none;
+            border: 1px solid black;
+            font-size: 1.2rem;
         }
     }
 
--- src/components/ProjectDetailForm/index.js
+++ src/components/ProjectDetailForm/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { FormContainer } from './ProjectDetailStyles'
+import MobileLogo from '../../assets/nav_logo_sharaway_mobile.png';
 
 const ProjectDetailForm = () => {
   return (
       <>
           <FormContainer>
             <header>
-               <div class="form-image">SSDC</div> 
+               <div class="form-image"><img src={MobileLogo} alt="mobile-logo"/></div> 
                 <div class="form-details-wrapper">
                     <h1>Project Details Form</h1>
                     <p>Please fill out the information form below and hit "submit" to send these details to Carolyn personally</p>
                 </div>
             </header>
+          <form>
+             <input type="text" name="fname" value="" placeholder="First Name"/> 
+             <input type="text" name="fname" value="" placeholder="Last Name"/> 
+          </form>
           </FormContainer>
       </>
   )

2fe6ab434c69565c83ad534f19da209c464bcdfc
Logia <carnoldcoding@gmail.com>
2025-01-24 16:43:47 -0500

    Prototype practically finished

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './index.css'
+
+const mobileNav = {
+    isOpen: false,
+    element: document.querySelector('#mobile-nav') as HTMLElement,
+    icon: document.querySelector('#mobile-nav-icon') as HTMLHtmlElement,
+    toggle: function() {
+        this.isOpen = this.isOpen ? false : true;
+    }
+};
+
+window.addEventListener('click', (e: MouseEvent)=> {
+    const target = e.target as HTMLElement;
+    console.log(target.classList)
+    if(target.id == "mobile-nav-icon" || target.id == "mobile-nav" || Array.from(target.classList).includes('nav-item')){
+        mobileNav.toggle();
+        if(mobileNav.isOpen){
+            mobileNav.element.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        }else if(!mobileNav.isOpen){
+            mobileNav.element.style.display = 'none';
+            document.body.style.overflow = 'unset';
+        }
+    }
+    
+})
